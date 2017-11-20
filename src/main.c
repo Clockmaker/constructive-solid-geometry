5eb8c760b787c643d926b2b9d317ca23428f06f5
Clockmaker <valegiuseppe@gmail.com>
2017-11-20 23:54:56 +0100

    added glad

--- src/main.c
+++ src/main.c
@@ -1,12 +1,16 @@
-#include <GLFW/glfw3.h>
 #include "def.h"
+#include "common.h"
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 #include "render.h"
 
 int main(void) {
-    printf("asf asd f");
     GLFWwindow* window;
     if (!glfwInit()) return -1;
 
+    //glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
+
+    
     window = glfwCreateWindow(640, 640, "CSG", NULL, NULL);
     if (!window) {
         glfwTerminate();
@@ -14,6 +18,12 @@ int main(void) {
     }
 
     glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) return -1;
+
+    DBG("%s (%s)", glGetString(GL_RENDERER), glGetString(GL_VENDOR));
+    DBG("OpenGL %s", glGetString(GL_VERSION));
+    DBG("GLSL %s", glGetString(GL_SHADING_LANGUAGE_VERSION));
 
     R_Init();
 
